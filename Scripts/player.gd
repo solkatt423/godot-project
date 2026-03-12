404ef506b70f84c6c1a2f29afe0944c1de784d2a
solkatt423 <3088584853@qq.com>
2026-03-12 14:37:53 +0800

    1

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -7,6 +7,13 @@ extends CharacterBody2D
 var is_game_over : bool = false
 
 @export var bullet_scene : PackedScene
+
+func _process(delta: float) -> void:
+	if velocity == Vector2.ZERO or is_game_over:
+		$runnningsound.stop()
+	elif not $runnningsound.playing:
+		$runnningsound.play()
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta: float) -> void:
 	if not is_game_over:
@@ -21,15 +28,29 @@ func _physics_process(delta: float) -> void:
 		move_and_slide()
 
 func game_over():
-	is_game_over = true
-	animator.play("game_over")
-	await get_tree().create_timer(3).timeout
-	get_tree().reload_current_scene()
+	if not is_game_over:
+		#设置游戏结束，播放失败动画
+		is_game_over = true
+		animator.play("game_over")
+		
+		get_tree().current_scene.show_game_over()
+		
+		$gameoversound.play()
+		
+		#等待3秒后重启游戏
+		$RestartTimer.start()
 
 
 func _on_fire() -> void:
 	if velocity != Vector2.ZERO or is_game_over:
 		return
+	
+	$firesound.play()
+	
 	var bullet_node = bullet_scene.instantiate()
 	bullet_node.position = position + Vector2(6,6)
 	get_tree().current_scene.add_child(bullet_node)
+
+
+func _reload_scene() -> void:
+	get_tree().reload_current_scene()
